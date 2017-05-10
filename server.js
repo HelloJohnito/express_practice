d3e11e22a3a291626ff9235b0d72e72a984d019d
John <johnitolee@gmail.com>
2017-05-09 22:28:08 -0700

    add ejs file

--- server.js
+++ server.js
@@ -8,13 +8,19 @@ app.get("/", function(req, res){
 });
 
 app.get("/bye", function(req,res){
-  res.send("Goodbye!");
+  res.send("Bye");
 });
 
 //anything can go inside subredditName.
 app.get("/r/:subredditName", function(req, res){
   var subreddit = req.params.subredditName;
   res.send(`${subreddit}`);
+});
+
+//render ejs page
+app.get("/ejs/:thing", function(req, res){
+  var thing = req.params.thing;
+  res.render("homepage.ejs", {thingVar: thing});
 });
 
 //everything else. Order of the routes matters here.
