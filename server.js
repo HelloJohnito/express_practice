00e08fcff4cf0a6de66020e9cfa53bc50b897a7a
John <johnitolee@gmail.com>
2017-05-15 21:18:41 -0700

    add post request

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
 // app.use(express.static(__dirname + '/public'));
 app.use(express.static("style"));
 //you don;t have to write ejs files in render
 app.set("view engine", "ejs");
 
+//tell express to use body parser
+app.use(bodyParser.urlencoded({extended: true}));
 ////////// Routes////////////////
+
+//get
 app.get("/", function(req, res){
   res.render("homepage"); //home.ejs is off bc app.set
 });
-
 
 //anything can go inside subredditName.
 app.get("/r/:subredditName", function(req, res){
   var subreddit = req.params.subredditName;
   res.send(`${subreddit}`);
 });
-
 
 //render ejs page
 app.get("/thing/:thing", function(req, res){
@@ -28,8 +31,23 @@ app.get("/thing/:thing", function(req, res){
 
 
 // everything else. Order of the routes matters here.
-app.get("*", function(req,res){
-  res.send("You are a star");
+// app.get("*", function(req,res){
+//   res.send("You are a star");
+// });
+
+var friends = ["John", "Aaron"];
+//get
+app.get('/friends', function(req, res){
+  res.render("friends", {friends: friends});
+});
+
+//POST//
+app.post("/addfriend", function(req, res){
+  //newfriend is from the name attr on input.
+  //req.body needs body parser.
+  var newFriend = req.body.newfriend;
+  friends.push(newFriend);
+  res.redirect("/friends");
 });
 
 // Tell Express to listen for requests (start server)
