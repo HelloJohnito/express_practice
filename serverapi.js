8b918dc8ad92a017f0c9937c0b4bc85c28544334
John <johnitolee@gmail.com>
2017-05-16 00:18:44 -0700

    adds movie api

--- serverapi.js
+++ serverapi.js
@@ -2,7 +2,16 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
+app.set("view engine", "ejs");
 
+app.get("/movies", function(req, res){
+  request("http://www.omdbapi.com/?s=star", function(error, response, body){
+    if(!error && response.statusCode === 200){
+      var data = JSON.parse(body);
+      res.render("movies", {movieData: data});
+    }
+  });
+});
 
 
 app.listen(3000, function(){
