8b8ae3b44beeae52e874c19054c9caef3b591fb9
John <johnitolee@gmail.com>
2017-05-27 16:02:11 -0700

    set up passports into app

--- auth/app.js
+++ auth/app.js
@@ -1,10 +1,29 @@
 var express = require("express");
 var mongoose = require("mongoose");
+var passport = require("passport");
+var bodyParser = require("body-parser");
+var LocalStrategy = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
+var expressSession = require("express-session");
+var User = require("./models/user");
 
 var app = express();
 mongoose.connect("mongodb://localhost/auth_demo");
 
 app.set('view engine', 'ejs');
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(expressSession({
+  secret: "we use this sentence to decode the session",
+  resave: false,
+  saveUninitialized: false
+}));
+
+// responsible for reading the session, taking the data and decoding the session. Then serialize it and pu it back
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/", function(req, res){
   res.render("home");
